2f311f9f1519ba0c98d0f1c5cd7017db4f307468
Rafael <rafael.jose1201@gmail.com>
2020-03-24 14:38:34 -0300

    Adding styled-components

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -10,41 +10,40 @@ import {
   selectCartTotal
 } from '../../redux/cart/cart.selectors';
 
-import './checkout.styles.scss';
+import {
+  CheckoutPageContainer,
+  CheckoutHeaderContainer,
+  HeaderBlockContainer,
+  TotalContainer,
+  TestWarningContainer
+} from './checkout.styles';
 
-const CheckoutPage = ({ cartItems, total }) => (
-  <div className="checkout-page">
-    <div className="checkout-header">
-      <div className="header-block">
-        <span>Product</span>
-      </div>
-      <div className="header-block">
-        <span>Description</span>
-      </div>
-      <div className="header-block">
-        <span>Quantity</span>
-      </div>
-      <div className="header-block">
-        <span>Price</span>
-      </div>
-      <div className="header-block">
-        <span>Remove</span>
-      </div>
-    </div>
-    {cartItems.map(cartItem => (
-      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
-    ))}
-    <div className="total">
-      <span>Total: ${total}</span>
-    </div>
-    <div className="test-warning">
-      *Please use the following test credit card for payments*
-      <br />
-      4242 4242 4242 4242 - Exp: 01/21 CVV:123
-    </div>
-    <StripeCheckoutButton price={total} />
-  </div>
-);
+const CheckoutPage = ({ cartItems, total }) => {
+  const headers = ['Product', 'Description', 'Quantity', 'Price', 'Remove'];
+  return (
+    <CheckoutPageContainer>
+      <CheckoutHeaderContainer>
+        {headers.map(header => (
+          <HeaderBlockContainer>
+            <span>{header}</span>
+          </HeaderBlockContainer>
+        ))}
+      </CheckoutHeaderContainer>
+      {cartItems.map(cartItem => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
+      <TotalContainer>
+        <span>Total: ${total}</span>
+      </TotalContainer>
+      <TestWarningContainer>
+        *Please use the following test credit card for payments*
+        <br />
+        4242 4242 4242 4242 - Exp: 01/21 CVV:123
+      </TestWarningContainer>
+      <StripeCheckoutButton price={total} />
+    </CheckoutPageContainer>
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
